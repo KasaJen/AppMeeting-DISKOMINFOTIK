0c299435b97acfdc054a115f2ed80b778fc2dccc
KasaJen <rezarevaldy007@gmail.com>
2026-04-05 12:21:19 +0800

    Minor change

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,10 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (config('app.env') !== 'local') {
-            \URL::forceScheme('https');
-        }
-
+        \URL::forceScheme('https');
         Paginator::useBootstrap();
 
         Schema::defaultStringLength(191);
